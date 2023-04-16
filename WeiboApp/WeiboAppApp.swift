5210477855eb848f1b499f03f2e5ccabb98d60fc
Jiaming Guo <j23guo@gmail.com>
2023-04-16 17:50:20 -0400

    Initial Commit

---
+++ WeiboApp/WeiboAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeiboAppApp.swift
+//  WeiboApp
+//
+//  Created by Jiaming Guo on 2023-04-16.
+//
+
+import SwiftUI
+
+@main
+struct WeiboAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
